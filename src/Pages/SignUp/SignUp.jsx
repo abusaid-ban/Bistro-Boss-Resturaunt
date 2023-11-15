ef8c130807cdc08e5a78c19950d3c16ea9610036
unknown <personal.abusaid@gmail.com>
2023-11-15 10:04:48 +0600

    new code added

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -9,6 +9,7 @@ const SignUp = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
     const {createUser} = useContext(AuthContext);
     const onSubmit = data => {
+        console.log(data);
         
         createUser(data.email,data.password)
         .then(result =>{
@@ -19,7 +20,7 @@ const SignUp = () => {
     return (
       <>
         <Helmet>
-                <title>Bistro Boss | SignUp</title>
+                <title>RoadSide Kitchen | SignUp</title>
             </Helmet>
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row-reverse">
@@ -47,7 +48,7 @@ const SignUp = () => {
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" {...register("password",{ required: true,
+                            <input type="password" {...register("password", { required : true,
                             minLength:6,
                             maxLength: 20,
                             pattern: /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])/
@@ -55,15 +56,13 @@ const SignUp = () => {
                             {errors.password?.type === 'minLength' && <p className="text-red-600 p-1">Password must be 6 characters</p>}
                             {errors.password?.type === 'maxLength' && <p className="text-red-600 p-1">Password must be less than 20 characters</p>}
                             {errors.password?.type === 'pattern' && <p className="text-red-600 p-1">Password must be one uppercase, one lowercase, one number</p>}
-                            <label className="label">
-                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                            </label>
+                           
                         </div>
                         <div className="form-control mt-6">
-                            <button className="btn btn-primary">Login</button>
+                            <button className="btn btn-primary">Sign Up</button>
                         </div>
                     </form>
-                    <p className='text-center pb-3'><small>Already have an Account? <Link className='text-blue-600' to="/login">Login Here</Link></small></p>
+                    <p className='text-center pb-3'>Already have an Account? <Link className='text-blue-600' to="/login">Login </Link>here</p>
                 </div>
             </div>
         </div>
