6e0b439032df047a804f4a7b7ff6ea33acf74e8b
unknown <personal.abusaid@gmail.com>
2023-12-11 15:10:24 +0600

    clicked button activated when added on cart

--- src/Components/FoodCart/FoodCart.jsx
+++ src/Components/FoodCart/FoodCart.jsx
@@ -3,29 +3,36 @@ import { useContext } from "react";
 import { AuthContext } from "../../Provider/AuthProviders";
 import Swal from "sweetalert2";
 import { useLocation, useNavigate } from "react-router-dom";
+import useCart from "../../Hooks/useCart";
 
 const FoodCart = ({ item }) => {
-    const { name, image, recipe, price,_id } = item;
+    const { name, image, recipe, price, _id } = item;
     const { user } = useContext(AuthContext);
+    const [ , refetch] = useCart();
     const navigate = useNavigate();
     const location = useLocation();
     const handleAddToCart = (item) => {
         console.log(item);
         if (user && user.email) {
-            const orderItem = {menuItemId : _id, name,image,price,email: user.email}
-            fetch('http://localhost:5000/carts',{
+            const orderItem = { menuItemId: _id, name, image, price, email: user.email }
+            fetch('http://localhost:5000/carts', {
                 method: 'POST',
-                headers:{
-                    'content-type' : 'application/json'
+                headers: {
+                    'content-type': 'application/json'
                 },
-                body: JSON.stringify( orderItem )
+                body: JSON.stringify(orderItem)
             })
                 .then(res => res.json())
                 .then(data => {
                     if (data.insertedId) {
+                        refetch();
+
                         Swal.fire({
-                            title: "Food Added on Cart Successfully!",
-                            icon: "success"
+                            position: 'center-end',
+                            title: "Food Added on Cart!",
+                            icon: "success",
+                            showConfirmButton: false,
+                            timer: 1500,
                         });
 
                     }
@@ -34,19 +41,19 @@ const FoodCart = ({ item }) => {
 
 
         }
-        else{
+        else {
             Swal.fire({
                 title: "Please login for order food",
-               icon: "warning",
+                icon: "warning",
                 showCancelButton: true,
                 confirmButtonColor: "#3085d6",
                 cancelButtonColor: "#d33",
                 confirmButtonText: "Login Now!"
-              }).then((result) => {
+            }).then((result) => {
                 if (result.isConfirmed) {
-                 navigate("/login",{state: { from :location}})
+                    navigate("/login", { state: { from: location } })
                 }
-              });
+            });
         }
     }
 
