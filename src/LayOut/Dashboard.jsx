f008624f3f69460e8a28a9b2f98c36ad1d5e6ac8
unknown <personal.abusaid@gmail.com>
2023-12-14 18:59:08 +0600

    check up new daisy ui version

--- src/LayOut/Dashboard.jsx
+++ src/LayOut/Dashboard.jsx
@@ -1,22 +1,21 @@
-import {  FaShoppingCart } from "react-icons/fa";
-import { Link, Outlet } from "react-router-dom";
+
+import {  Outlet } from "react-router-dom";
 
 const Dashboard = () => {
     return (
        
-        <div className="drawer lg:drawer-open">
+        <div className="drawer drawer-mobile">
         <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content flex flex-col items-center justify-center">
-          {/* Page content here */}
-          <Outlet></Outlet>
+         <Outlet></Outlet>
           <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
         
         </div> 
         <div className="drawer-side">
-          <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label> 
-          <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-            {/* Sidebar content here */}
-            <li><Link><FaShoppingCart></FaShoppingCart></Link></li>
+          <label htmlFor="my-drawer-2" className="drawer-overlay"></label> 
+          <ul className="menu p-4 w-80 bg-base-100 text-base-content">
+           
+            <li><a>Sidebar Item 1</a></li>
             <li><a>Sidebar Item 2</a></li>
           </ul>
         
