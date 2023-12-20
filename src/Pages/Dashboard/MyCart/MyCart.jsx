4b5e9f38f8891bb0c215f48bf8e372a1a36cee4f
unknown <personal.abusaid@gmail.com>
2023-12-20 09:56:16 +0600

    count total on my cart section

--- src/Pages/Dashboard/MyCart/MyCart.jsx
+++ src/Pages/Dashboard/MyCart/MyCart.jsx
@@ -1,9 +1,19 @@
+import { Helmet } from "react-helmet-async";
+import useCart from "../../../Hooks/useCart";
 
 
 const MyCart = () => {
+    const [cart] = useCart();
+    const total = cart.reduce((sum,item)=> item.price + sum , 0)
     return (
         <div>
-            <h2> My CART</h2>
+             <Helmet>
+                <title>RoadSite Kitchen | My Cart</title>
+            </Helmet>
+           <div>
+            <h2 className="text-3xl">Total products: {cart.length}</h2>
+            <h2 className="text-3xl">Total Price: {total}</h2>
+           </div>
             
         </div>
     );
